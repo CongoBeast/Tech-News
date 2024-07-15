a923fb0f4dbe3c8237bb0ae222ad773bcde2fa8a
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-07-15 13:21:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Admin from './components/Admin';
 import Funding from './components/Funding';
 import StartupCard from './components/startupCard';
 import FundingAnalysis from './components/FundingAnalysis';
+import ArticlesPage from './components/ArticlesPages';
+import FundingPage from './components/FundingPage';
 import ArticleEntry from './components/ArticleEntry';
 import FundingEntry from './components/fundingEntry'; // Import the ArticleEntry component
 import RegionArticles from './components/RegionArticles';
@@ -32,6 +34,14 @@ function App() {
             <Route path="/funding" element={<Funding />} />
             <Route path="/fundinganalysis" element={<FundingAnalysis />} />
             <Route path="/startup/:name" element={<StartupCard />} />
+
+            <Route path='/funding-page' element={<FundingPage />}/>
+            <Route path='/articles-pages' element={<ArticlesPage/>}/>
+
+            <Route path="/edit-article/:id" component={ArticleEntry} />
+            <Route path="/edit-funding/:id" component={FundingEntry} />
+
+
             <Route path="/auth" element={<AuthPage />} />
             <Route path="/article-entry" element={<ArticleEntry />} />
             <Route path="/funding-entry" element={<FundingEntry />} /> {/* Add the route for the article entry page */}

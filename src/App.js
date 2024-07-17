d6e4ea5b5a0508b2df2b7c51a779eba5d4e4b7a8
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-07-17 13:03:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { HashRouter, Routes, Route, Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/styles.css';
 import Sidebar from './components/Sidebar';
@@ -18,43 +18,45 @@ import FundingEntry from './components/fundingEntry'; // Import the ArticleEntry
 import RegionArticles from './components/RegionArticles';
 import AuthPage from './components/AuthPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import TopNavBar from "./components/TopNavBar";
 
 
 function App() {
   return (
-  <HashRouter>
+    <HashRouter> {" "}
       <div className="d-flex">
         <Sidebar />
+        
         <div className="container-fluid">
+        <TopNavBar />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/trends" element={<Trends />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/region" element={<Region />} />
+            {/* <Route path="/admin" element={<Admin />} /> */}
+            <Route path="/funding" element={<Funding />} />
+            <Route path="/fundinganalysis" element={<FundingAnalysis />} />
+            <Route path="/startup/:name" element={<StartupCard />} />
 
-              <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/trends" element={<Trends />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/region" element={<Region />} />
-                {/* <Route path="/admin" element={<Admin />} /> */}
-                <Route path="/funding" element={<Funding />} />
-                <Route path="/fundinganalysis" element={<FundingAnalysis />} />
-                <Route path="/startup/:name" element={<StartupCard />} />
-    
-                <Route path='/funding-page' element={<FundingPage />}/>
-                <Route path='/articles-pages' element={<ArticlesPage/>}/>
-    
-                <Route path="/edit-article/:id" component={ArticleEntry} />
-                <Route path="/edit-funding/:id" component={FundingEntry} />
-    
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/admin" element={<Admin />} />
-                </Route>
-    
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/article-entry" element={<ArticleEntry />} />
-                <Route path="/funding-entry" element={<FundingEntry />} /> {/* Add the route for the article entry page */}
-                <Route path="/region/:region" element={<RegionArticles />} />  {/* Add RegionArticles route */}
-              </Routes>
+            <Route path='/funding-page' element={<FundingPage />}/>
+            <Route path='/articles-pages' element={<ArticlesPage/>}/>
+
+            <Route path="/edit-article/:id" component={ArticleEntry} />
+            <Route path="/edit-funding/:id" component={FundingEntry} />
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/admin" element={<Admin />} />
+            </Route>
+
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/article-entry" element={<ArticleEntry />} />
+            <Route path="/funding-entry" element={<FundingEntry />} /> {/* Add the route for the article entry page */}
+            <Route path="/region/:region" element={<RegionArticles />} />  {/* Add RegionArticles route */}
+          </Routes>
         </div>
       </div>
-    </HashRouter>
+      </HashRouter>
   );
 }
 

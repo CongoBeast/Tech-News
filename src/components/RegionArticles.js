b5e0503146aa8dc676114b9a24094e716ed83703
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-08-21 21:06:35 +0200

    Add files via upload

--- src/components/RegionArticles.js
+++ src/components/RegionArticles.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Row, Col, Card, Button, Dropdown, Modal , Breadcrumb, Badge, Stack } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Dropdown, Modal, Breadcrumb, Badge, Stack, Spinner } from 'react-bootstrap';
 import { useParams, Link } from 'react-router-dom';
 import './RegionArticles.css';
 
@@ -12,22 +12,27 @@ function RegionArticles() {
   const monthString = today.toLocaleDateString('en-US', { month: 'long' });
   const year = today.getFullYear();
   const [filter, setFilter] = useState('thisMonth');
-
   const [articles, setArticles] = useState([]);
   const { region } = useParams();
   const [showModal, setShowModal] = useState(false);
   const [selectedPeriod, setSelectedPeriod] = useState(`${monthString} ${year}, Week ${Math.ceil(today.getDate() / 7)}`);
   const [activeGenre, setActiveGenre] = useState('All');
-
   const [selectedWeek, setSelectedWeek] = useState(Math.ceil(today.getDate() / 7));
   const [selectedMonth, setSelectedMonth] = useState(monthString);
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  const [analysisVisibility, setAnalysisVisibility] = useState({});
+
+
+  // New state variables for analysis and loading
+  const [analysis, setAnalysis] = useState({});
+  const [loading, setLoading] = useState({});
 
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
 
   const handlePeriodChange = (month, week, year) => {
-    setFilter('all')
+    setFilter('all');
     setSelectedMonth(month);
     setSelectedWeek(week);
     setSelectedYear(year);
@@ -41,7 +46,6 @@ function RegionArticles() {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };
-
 
   useEffect(() => {
     fetchArticles();
@@ -57,6 +61,20 @@ function RegionArticles() {
       });
   };
 
+  const handleAnalysis = async (article, idx) => {
+    setLoading(prevLoading => ({ ...prevLoading, [idx]: true }));
+    
+    try {
+      const response = await axios.post('https://cordelia-analyze.onrender.com/analyze', { documents: [article] });
+      setAnalysis(prevAnalysis => ({ ...prevAnalysis, [idx]: response.data.analysis }));
+      setAnalysisVisibility(prevState => ({ ...prevState, [idx]: true }));
+    } catch (error) {
+      console.error('There was an error fetching the analysis!', error);
+    } finally {
+      setLoading(prevLoading => ({ ...prevLoading, [idx]: false }));
+    }
+  };
+
   var filteredArticles = articles.filter(article => {
     const articleDate = new Date(article.date);
     const isActiveGenre = activeGenre === 'All' || article.tag.includes(activeGenre);
@@ -67,24 +85,31 @@ function RegionArticles() {
     return isActiveGenre && isInSelectedPeriod;
   });
 
-  filteredArticles = filteredArticles.filter(article => article.region === region);
+  filteredArticles = filteredArticles.filter(article => article.region === region).reverse();
+
+  const toggleAnalysisVisibility = (articleId) => {
+    setAnalysisVisibility(prevState => ({
+      ...prevState,
+      [articleId]: !prevState[articleId],
+    }));
+  };
+  
 
   return (
     <Container fluid className="region-articles-container">
 
-            {/* Breadcrumbs */}
+      {/* Breadcrumbs */}
       <Breadcrumb className='p-3 rounded'>
         <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/' }}>
           <Badge bg="primary">Home</Badge>
         </Breadcrumb.Item>
         <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/' }}>
-        <Badge bg="primary">Regions</Badge>
+          <Badge bg="primary">Regions</Badge>
         </Breadcrumb.Item>
         <Breadcrumb.Item active>
-           <Badge bg="dark">{region.charAt(0).toUpperCase() + region.slice(1)}</Badge>
+          <Badge bg="dark">{region.charAt(0).toUpperCase() + region.slice(1)}</Badge>
         </Breadcrumb.Item>
       </Breadcrumb>
-
 
       <h2 className="my-4 text-center">{region.charAt(0).toUpperCase() + region.slice(1)}</h2>
       <p className="text-center">{selectedPeriod}</p>
@@ -130,9 +155,93 @@ function RegionArticles() {
                 <div className="d-flex justify-content-between align-items-center">
                   <span className="text-muted">{new Date(article.date).toLocaleDateString()}</span>
                 </div>
-                <a href={article.link} className="btn btn-link p-0 mt-2">
-                  Read full article
-                </a>
+
+                {/* <Button
+                  variant="primary"
+                  onClick={() => handleAnalysis(article, idx)}
+                  disabled={loading[idx]}
+                >
+                  {loading[idx] ? <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" /> : 'Quick analysis'}
+                </Button>
+
+                {analysis[idx] && (
+                  <div className="mt-3">
+                    <strong>Analysis:</strong>
+                    <p>{analysis[idx]}</p>
+                  </div>
+                )} */}
+                {loading[idx] ? (
+              <Button 
+                variant="secondary" 
+                disabled 
+                style={{
+                  display: 'block',
+                  margin: '10px auto',
+                  fontSize: '0.8rem',
+                  padding: '0.25rem 0.5rem'
+                }}>
+                <Spinner 
+                  as="span" 
+                  animation="border" 
+                  size="sm" 
+                  role="status" 
+                  aria-hidden="true" 
+                />
+                Loading...
+              </Button>
+            ) : (
+              <Button 
+                variant="primary" 
+                onClick={() => handleAnalysis(article, idx)}
+                style={{
+                  display: 'block',
+                  margin: '10px auto',
+                  background: 'linear-gradient(90deg, rgba(58,123,213,1) 0%, rgba(52,82,242,1) 50%, rgba(79,110,249,1) 100%)',
+                  border: 'none',
+                  fontSize: '0.8rem',
+                  padding: '0.25rem 0.5rem'
+                }}>
+                Quick analysis
+              </Button>
+            )}
+
+            {analysis[idx] && analysisVisibility[idx] && (
+              <div className="mt-3">
+                <strong>Analysis:</strong>
+                <p>{analysis[idx]}</p>
+                <Button 
+                  variant="outline-secondary" 
+                  onClick={() => toggleAnalysisVisibility(idx)}
+                  style={{
+                    display: 'block',
+                    margin: '10px auto',
+                    fontSize: '0.8rem',
+                    padding: '0.25rem 0.5rem',
+                    backgroundColor: 'black',
+                    color: 'white'
+                  }}>
+                  Hide
+                </Button>
+              </div>
+            )}
+
+{analysis[idx] && !analysisVisibility[idx] && (
+  <Button 
+    variant="outline-secondary" 
+    onClick={() => toggleAnalysisVisibility(idx)}
+    style={{
+      display: 'block',
+      margin: '10px auto',
+      fontSize: '0.8rem',
+      padding: '0.25rem 0.5rem',
+      backgroundColor: 'green',
+      color: 'white'
+    }}>
+    Show Analysis
+  </Button>
+)}
+
+
 
                 {article.keyWords && <Stack direction="horizontal" gap={2} className='mt-2'>
                 {article.keyWords.map((keyword, index) => (
@@ -191,10 +300,15 @@ function RegionArticles() {
               ))}
             </Dropdown.Menu>
           </Dropdown>
-          <Button variant="primary" className="mt-3" onClick={() => handlePeriodChange(selectedMonth, selectedWeek, selectedYear)}>
-            Confirm Period
-          </Button>
         </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handlePeriodChange(selectedMonth, selectedWeek, selectedYear)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
       </Modal>
     </Container>
   );
